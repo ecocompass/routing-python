6c42592155696ece87ba0fa4f8c48474b1c361b3
Anupal Mishra <anupal.pm@gmail.com>
2024-03-21 18:50:22 +0000

    dart preprocessing workaround

--- preprocessing/transit.py
+++ preprocessing/transit.py
@@ -35,6 +35,11 @@ def compute_route_and_stops(gtfs_type):
     for _, row in tqdm(merged_df.iterrows(), total=merged_df.shape[0], desc="Processing route timings"):
         stop_id = row['stop_id']
         route_short_name = row['route_short_name']
+
+        # skip all rails other than dart
+        if gtfs_type == "dart" and route_short_name != "DART":
+            continue
+
         service_id = row['service_id']
 
         if route_short_name not in data_structure[f"{gtfs_type}_stops"][stop_id]["routes"]:
@@ -46,9 +51,24 @@ def compute_route_and_stops(gtfs_type):
         data_structure[f"{gtfs_type}_stops"][stop_id]["routes"][route_short_name][service_id].append(
             row['arrival_time'])
 
+    if gtfs_type == "dart":
+        cleaned_stops = {}
+        # cleaning up empty stops for DART
+        for stop_id in data_structure[f"{gtfs_type}_stops"]:
+            if data_structure[f"{gtfs_type}_stops"][stop_id]["routes"]:
+                cleaned_stops[stop_id] = data_structure[f"{gtfs_type}_stops"][stop_id]
+
+        data_structure[f"{gtfs_type}_stops"] = cleaned_stops
+
+
     print(f"\nAdding '{gtfs_type}' route entries.", flush=True)
     for _, row in tqdm(merged_df.iterrows(), total=merged_df.shape[0], desc="Processing route entries"):
         route_short_name = row['route_short_name']
+
+        # skip all rails other than dart
+        if gtfs_type == "dart" and route_short_name != "DART":
+            continue
+
         route_key = f"{route_short_name}_{row['direction_id']}"
         if route_key not in data_structure[f"{gtfs_type}_routes"]:
             data_structure[f"{gtfs_type}_routes"][route_key] = []
@@ -63,12 +83,20 @@ def compute_route_and_stops(gtfs_type):
 
             for route_key in luas_workaround:
                 data_structure[f"{gtfs_type}_shapes"][route_key] = luas_workaround[route_key]
+    elif gtfs_type == "dart":
+        with open("data/.dart_shapes.workaround.json", "r") as file:
+            dart_workaround = json.loads(file.read())
+
+            for route_key in dart_workaround:
+                data_structure[f"{gtfs_type}_shapes"][route_key] = dart_workaround[route_key]
+
     else:
         for _, row in tqdm(shapes_df.iterrows(), total=shapes_df.shape[0], desc="Processing route shapes"):
             shape_id = row['shape_id']
             # Find a trip that uses this shape_id to get route_short_name and direction_id
             trip_row = trips_df[trips_df['shape_id'] == shape_id].iloc[0]
             route_short_name = routes_df[routes_df['route_id'] == trip_row['route_id']].iloc[0]['route_short_name']
+
             route_key = f"{route_short_name}_{trip_row['direction_id']}"
 
             if route_key not in data_structure[f"{gtfs_type}_shapes"]:
